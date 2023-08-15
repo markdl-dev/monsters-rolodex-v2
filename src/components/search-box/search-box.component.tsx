a6d1e39de7c8490c87e4be44419b9f38df0d0b6b
Mark DL <mdl.softwareengineer@gmail.com>
2023-08-16 02:49:43 +0800

    TYPSCRIPT - Interface examples

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -2,7 +2,30 @@ import './search-box.styles.css';
 
 // Never use the ANY keyword
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+// Type vs Interface
+// Interface is extendable, similar to class components
+// - you can extend class components to another and inherit all of the methods
+
+interface ISearchBoxProps {
+  className: string;
+  placeholder?: string; // optional ? (string or null)
+}
+
+// extend the interface
+// interface ISearchBoxProps extends IOnChangeHandler {
+//   className: string;
+//   placeholder?: string; // optional ? (string or null)
+// }
+// interface IOnChangeHandler {
+//   onChangeHandler: (a: string) => void
+// }
+
+// overload the interface
+// interface ISearchBoxProps {
+//   onChangeHandler: (a: string) => void
+// }
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: ISearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type='search'
