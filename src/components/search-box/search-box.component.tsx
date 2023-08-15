ba5ce7d348b95e2a1d3d0b21ae282790718a299b
Mark DL <mdl.softwareengineer@gmail.com>
2023-08-16 02:56:25 +0800

    TYPSCRIPT - TYPE examples

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -2,30 +2,41 @@ import './search-box.styles.css';
 
 // Never use the ANY keyword
 
-// Type vs Interface
-// Interface is extendable, similar to class components
-// - you can extend class components to another and inherit all of the methods
-
 interface ISearchBoxProps {
   className: string;
   placeholder?: string; // optional ? (string or null)
 }
 
-// extend the interface
-// interface ISearchBoxProps extends IOnChangeHandler {
+// What is the difference?
+// Types allows union
+// Union - combination of types
+// Golden rule - function programming use types, OOP use interface
+
+// type SearchBoxProps = {
 //   className: string;
-//   placeholder?: string; // optional ? (string or null)
-// }
-// interface IOnChangeHandler {
+//   placeholder?: string;
 //   onChangeHandler: (a: string) => void
 // }
-
-// overload the interface
-// interface ISearchBoxProps {
-//   onChangeHandler: (a: string) => void
+//
+// type CanadianAddress = {
+//   street: string;
+//   province: string;
+// }
+//
+// type USAddress = {
+//   street: string;
+//   state: string;
+// }
+//
+// type Address = CanadianAddress | USAddress;
+//
+// const Address: Address = {
+//   street: 'test',
+//   state: 'test',
+//   province: 'test'
 // }
 
-const SearchBox = ({ className, placeholder, onChangeHandler }: ISearchBoxProps) => (
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
   <input
     className={`search-box ${className}`}
     type='search'
